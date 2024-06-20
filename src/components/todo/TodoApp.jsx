287cbd1d2b30fb0345f3492db86b819ec6f2b945
Harsh Vardhan Choudhary <choudharyharsh964@gmail.com>
2024-06-20 16:05:00 +0530

    adding bootstrap to the react application

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -6,8 +6,8 @@ import {BrowserRouter, Routes, Route, useNavigate, useParams, Link} from 'react-
 export default function TodoApp() {
     return (
         <div className="TodoApp">
-            <HeaderComponent/>
             <BrowserRouter>
+            <HeaderComponent/>
                 <Routes>
                     <Route path='/' element={<LoginComponent/>} /> 
                     <Route path='/login' element={<LoginComponent/>} /> 
@@ -16,8 +16,8 @@ export default function TodoApp() {
                     <Route path='*' element={<ErrorComponent/>} /> 
                     <Route path='/logout' element={<LogoutComponent/>} /> 
                 </Routes>
-            </BrowserRouter>
             <FooterComponent/>
+            </BrowserRouter>
         </div>
     )
 }
@@ -144,10 +144,10 @@ function ListTodoComponent() {
     ]
 
     return (
-        <div className="ListTodoComponent">
+        <div className="container">
             <h1>Things you want to do</h1>
             <div>
-                <table>
+                <table className="table">
                     <thead>
                         <tr>
                             <td>Id</td>
@@ -182,17 +182,36 @@ function ListTodoComponent() {
 
 function HeaderComponent() {
     return (
-        <div className="header">
-            Header <hr/>
+        <header className="border-bottom border-light border-5 mb-5 p-2">
+        <div className="container">
+            <div className="row">
+                <nav className="navbar navbar-expand-lg">
+                    <a className="navbar-brand ms-2 fs-2 fw-bold text-black" href="https://www.linkedin.com/in/harsh-vardhan-choudhary-19b24b220/">Harsh</a>
+                    <div className="collapse navbar-collapse">
+                        <ul className="navbar-nav">
+                            <li className="nav-item fs-5"><Link className="nav-link" to="/welcome/harsh">Home</Link></li>
+                            <li className="nav-item fs-5"><Link className="nav-link" to="/todos">Todos</Link></li>
+                        </ul>
+                    </div>
+                    <ul className="navbar-nav">
+                        <li className="nav-item fs-5"><Link className="nav-link" to="/login">Login</Link></li>
+                        <li className="nav-item fs-5"><Link className="nav-link" to="/logout">Logout</Link></li>
+                    </ul>
+                </nav>
+            </div>
         </div>
+    </header>
+
     )
 }
 
 function FooterComponent() {
     return (
-        <div className="footer">
-            <hr/> Footer
-        </div>
+        <footer className="footer">
+            <div className="container">
+                Your footer
+            </div>
+        </footer>
     )
 }
 
